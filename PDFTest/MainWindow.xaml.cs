b344c518cea6af42562124778ead3ee510f940e7
sat <shaty3@hotmail.com>
2020-09-24 13:01:05 +0900

    プロジェクト ファイルを追加します。

---
+++ PDFTest/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace PDFTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var filename = Path.Combine(currentDirectory, "cobas2.png");
+            var uri = new Uri(filename);
+            var html = string.Format(File.ReadAllText("template.html"), uri.AbsoluteUri);
+            File.WriteAllText("temp.html", html);
+            PDF.HtmlToPdf("temp.html", "test.pdf");
+            PDF.OpenPdf("test.pdf");
+        }
+    }
+}
---
+++ PDFTest/PDF.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace PDFTest
+{
+    public class PDF
+    {
+        /// <summary>
+        /// HTML→PDF変換
+        /// </summary>
+        /// <param name="inputFilename"></param>
+        /// <param name="outputFilename"></param>
+        public static void HtmlToPdf(string inputFilename, string outputFilename)
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "wkhtmltopdf.exe",
+                Arguments = $"-O Landscape -n --enable-local-file-access {inputFilename} {outputFilename}",
+                UseShellExecute = true,
+                CreateNoWindow = false,
+            });
+        }
+
+        /// <summary>
+        /// PDFを開く
+        /// </summary>
+        /// <param name="pdfFilename"></param>
+        public static void OpenPdf(string pdfFilename)
+        {
+            Process.Start("cmd.exe", $"/c {pdfFilename}");
+        }
+    }
+}
